c8f31ce71a1a688161b468ca7e710c1688d16369
naspredam <mnaspreda@gmail.com>
2020-11-06 19:36:56 -0300

    Recover the object validator tests

--- src/test/java/com/example/service/user/infrastructure/validator/ObjectValidatorTest.java
+++ src/test/java/com/example/service/user/infrastructure/validator/ObjectValidatorTest.java
@@ -1,6 +1,36 @@
 package com.example.service.user.infrastructure.validator;
 
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.constraints.NotNull;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ObjectValidatorTest {
 
+    private static class TestObject {
+
+        @NotNull
+        private final Long id;
+
+        private TestObject(Long id) {
+            this.id = id;
+        }
+    }
+
+    @Test
+    public void shouldThrowException_whenSomeFieldsAreNotPassingAnnotationValidations() {
+        TestObject testObject = new TestObject(null);
+
+        assertThatThrownBy(() -> ObjectValidator.validate(testObject))
+                .isInstanceOf(ConstraintViolationException.class);
+    }
+
+    @Test
+    public void shouldPassValidationWithNoException_whenAllFieldsPassesAnnotationValidations() {
+        TestObject testObject = new TestObject(11L);
+
+        ObjectValidator.validate(testObject);
+    }
 }
