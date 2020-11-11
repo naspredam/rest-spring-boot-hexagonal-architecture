4421cf28ba31fe2e8c82271e9b82eead649d124d
naspredam <mnaspreda@gmail.com>
2020-11-11 19:45:40 -0300

    Do not clash with spring-boot main class

---
+++ src/main/java/com/example/service/user/UserSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.service.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSpringBootApplication.class, args);
+	}
+
+}
